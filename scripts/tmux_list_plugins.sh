2b10c07ba7c0c75506ee48031970a1992f487880
sebastien3k <thirdcoastchi@gmail.com>
2026-02-06 12:24:05 -0600

    replacing plugin list logic with popup test

--- scripts/tmux_list_plugins.sh
+++ scripts/tmux_list_plugins.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-# fetching the value of "tpm_plugins" option
-plugins_list=$(tmux show-option -gqv "@tpm_plugins")
-
-# displaying variable content, line by line
-for plugin in $plugins_list; do
-    echo $plugin
-done
+tmux display-popup -E -w 80% -h 80% \
+  "bash -c 'echo Testing popup; sleep 5; echo Closing...'"
